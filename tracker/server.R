31843b84d67fb59ba18a1d20abd3fd8019fa06de
Ryan Holl <ryan@inscisive.co.za>
2016-05-06 14:54:27 +0200

    simplify

--- tracker/server.R
+++ tracker/server.R
@@ -7,13 +7,26 @@ SEP <- read.csv("data/SEP.csv", stringsAsFactors = FALSE
                 #,
                 #fileEncoding="latin1"
                 )
+
+
 SEPactx <- read.csv("data/SEPactx.csv", stringsAsFactors = FALSE
                     #,
                     #fileEncoding="latin1"
                     )
 
+SEPclean_actx_list <- read.csv("data/SEPclean_actx_list.csv", stringsAsFactors = FALSE
+                               #,
+                               #fileEncoding="latin1"
+                                )
 
-Products <- merge(SEP, SEPactx, by = c('Nappi.Code'))
+SEPactx_mapping <- read.csv("data/SEPactx_mapping.csv", stringsAsFactors = FALSE
+                            #,
+                            #fileEncoding="latin1"
+                            )
+
+Mappedactx <- merge(SEPactx,SEPactx_mapping, by = "Active.Ingredients")
+
+Products <- merge(SEP, Mappedactx, by = c('Nappi.Code'))
 
 
 shinyServer(function(input, output) {
@@ -40,12 +53,20 @@ shinyServer(function(input, output) {
   
   output$text2 <- renderTable(
 
-    subset(Products, (Active.Ingredients %in% input$select))
+    subset(Products, (Active.Ingredients.2 %in% input$select))
     [ ,c(1,3,4,5,6,7,8,21,22,23,9,10,11,12,13,14,16,18,19)],
     include.rownames = FALSE
     
     )
   
+  output$list_name <- renderTable(
+    
+  lists <- data.frame(List.Name=input$list_name, Active.Ingredient=input$select)
+#   lists$Active.Ingredient <- output$list_name
+  
+     
+  )
+
   
   output$downloadData <- downloadHandler(
     filename = function() { 
@@ -54,7 +75,7 @@ shinyServer(function(input, output) {
     content = function(file) {
       write.csv(
         
-        subset(Products, (Active.Ingredients %in% input$select))
+        subset(Products, (Active.Ingredients.2 %in% input$select))
         [ ,c(1,3,4,5,6,7,8,21,9,10,11,12,13,14,16,18,19)],row.names = FALSE
         
         , file) 
--- tracker/ui.R
+++ tracker/ui.R
@@ -1,12 +1,12 @@
  
 # Single Exit Prices App
 
-SEPactx <- read.csv("data/SEPactx.csv", stringsAsFactors = FALSE
-                    #, 
-                    #fileEncoding="latin1"
-                    )
+SEPclean_actx_list <- read.csv("data/SEPclean_actx_list.csv", stringsAsFactors = FALSE
+                               #,
+                               #fileEncoding="latin1"
+)
 
-Listactx <- SEPactx[ ,c(2)]
+Listactx <- SEPclean_actx_list[ ,c(1)]
 Listactx <- Listactx[order(Listactx)]
 Listactx <- unique(Listactx)
 
@@ -21,46 +21,50 @@ shinyUI(fluidPage(
 #   h3 ("Background"),
 #   h5 ("Single Exit Price(SEP) refers to legislation mandating that medicine manufacturers may only sell their products at one price to all their customers, regardless of the nature of the customer’s order size and consumption levels. The term means that each and every product and its variants ‘exits’ the factory at one single price for that product or variant. Every drug on the market (and even some drugs not yet available in SA) will have an SEP. A drug’s single exit price is important to health economists and used for models that describe the costs of health care therapy."),
 #   
+    
 
+# sidebarLayout(
+# # sidebarPanel(
+#   
+#    
+#         textInput("sign_in", label = h5("Email Address"), 
+#                    value = ""),
+#         textInput("sign_in2", label = h5("Password"), 
+#                    value = ""),
+#         submitButton("Sign In")),
+
+# mainPanel(
+#  
+#   fluidRow(
+#     
+#     column(3, 
+#            textInput("list_name", label = h4("List Name"), 
+#                      value = "Enter list name..."))
+#   ),
   
-fluidRow(
-  
-  column(3, 
-         textInput("username", label = h5("Username"), 
-                   value = "Enter username..."))
-),
-
-fluidRow(
-
-  column(3, 
-         textInput("pass", label = h5("Password"), 
-                   value = "Enter password..."))
-),
-
-fluidRow(
-
+  fluidRow(
+    
     column(3,
            selectInput("select", label = h6("Begin typing active ingredient name and make selection"), 
-                       choices = list("Active_ingredient" = Listactx), selected = "Aspirin", selectize=FALSE, multiple=TRUE),
-            submitButton("Submit"))
-  ),
+                       choices = list("Active_ingredient" = Listactx), selected = "aspirin", selectize=FALSE, multiple=TRUE),
+           submitButton("Submit"))),
   
-   
+  
   br(),
-      textOutput("text0"),
+  textOutput("text0"),
   
   
-      textOutput("text1"),
-
-br(),
-
-downloadButton('downloadData', 'Download'),
+  textOutput("text1"),
   
   br(),
   
-      tableOutput("text2")
+  downloadButton('downloadData', 'Download'),
   
-    
-  ))
+  br(),
   
+  tableOutput("text2")
+  
+))
 
+
+  
